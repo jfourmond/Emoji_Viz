5356186447fd54053cc11fc618bc3b1cd0a7fa60
Jérôme FOURMOND <jerome@fourmond.fr>
2016-12-07 09:47:05 +0100

    Retouche nom de fichier

---
+++ retrieveTweetsOneHour.js
@@ -0,0 +1,42 @@
+// Récupération des Tweets toutes les 15 minutes pendant une heure
+//
+
+var Twitter = require('twitter-node-client').Twitter;
+var jsonFile = require('jsonfile');
+
+var json;
+var id;
+var count = 0;
+var MAX = 4;
+
+//Callback functions
+var error = function (err, response, body) {
+	console.log('ERROR [%O]', err);
+};
+
+var success = function (data) {
+	count ++;
+	json = JSON.parse(data);
+	console.log('Data [%O]', data);
+
+	var date = new Date();
+	var outputFile = 'data_test' + Date.now() + '.json';
+	jsonFile.writeFile(outputFile, json, {spaces: 4}, function (err) {
+		console.error(err)
+	});
+	if(count == 30) clearInterval(id);
+};
+
+var inputFile = 'twitter_config.txt';
+jsonFile.readFile(inputFile, function(err, obj) {
+	var config = obj;
+	// Premier lancement
+	search(config);
+	// Puis sur un intervalle régulier de 15 minutes
+	id = setInterval(search, 60 * 15000, config);
+});
+
+function search(config) {
+	var twitter = new Twitter(config);
+	twitter.getSearch({'q':'emoji', 'lang':'fr', 'count':'100'}, error, success);
+}
